37ef03912429b3ae09bf3a97005fd0c9e3c2ac13
Alan Evans <AlanWEvans@gmail.com>
2017-01-30 16:05:38 -0700

    Security Update, make --user/--password optional
    Passing passwords on the command line is a security risk.  Make
    --user/--password optional in favor of using ~/.pulp/client-cert.pem
    or ~/.pulp/admin.conf.
    
    errata_import.pl:
    * Make --user/--password optional and update usage
    * Add a warning about using --user/--password
    * Add $pulp_args and populate if --user/--password are used
    * If user and password are NOT supplied verify one of
      ~/.pulp/user-cert.pem or ~/.pulp/admin.conf are available
    
    sample_wrapper_script.sh:
    * show usage without user/password
    * show usage with pulp-admin repo list | aws | xargs
    
    README.md:
    * Update examples
    * Update for making --user/--password optional
    * Add section Authentication options

--- sample_wrapper_script.sh
+++ sample_wrapper_script.sh
@@ -5,7 +5,12 @@ cd /tmp
 wget -N http://cefs.steve-meier.de/errata.latest.xml
 wget -N https://www.redhat.com/security/data/oval/com.redhat.rhsa-all.xml
 
-PASSWORD=$(grep -i ^default_password /etc/pulp/server.conf | awk '{print $2}')
+# See README.md about setting up the credentials for pulp-admin
+# It his **highly** recommended that the password be passed on the commandline.
 
-/sbin/errata_import.pl --errata=/tmp/errata.latest.xml --rhsa-oval=/tmp/com.redhat.rhsa-all.xml --user=admin --password=$PASSWORD --debug --include-repo=Org-CentOS_7_x86_64-CentOS_7_x86_64_Base Org-CentOS_7_x86_64-CentOS_7_x86_64_Updates Org-CentOS_7_x86_64-CentOS_7_x86_64_Extras
+/sbin/errata_import.pl --errata=/tmp/errata.latest.xml --rhsa-oval=/tmp/com.redhat.rhsa-all.xml --debug --include-repo=Org-CentOS_7_x86_64-CentOS_7_x86_64_Base Org-CentOS_7_x86_64-CentOS_7_x86_64_Updates Org-CentOS_7_x86_64-CentOS_7_x86_64_Extras
 
+# If all of the repos that you want to import the errata into contain an indentifying pattern
+# in their labels you can use something like the following to ensure you don't miss new repos.
+
+# pulp-admin repo list --fields id | awk '/CentOS/ { print "--include-repo="$NF }' | xargs /sbin/errata_import.pl --errata=/tmp/errata.latest.xml --rhsa-oval=/tmp/com.redhat.rhsa-all.xml
